8493ee1ded0b71d2183a229622a0fd255011b170
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-04-18 13:37:56 +0600

    complete checkout page

--- src/Pages/Chcekout/Chcekout.js
+++ src/Pages/Chcekout/Chcekout.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import { Button, Col, Form, InputGroup, Row } from 'react-bootstrap';
 import { useLocation, useParams } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 import useService from '../../CustomHooks/useService';
+
+import checkout from '../../Images/cehckout.png'
 
 import './Chcekout.css';
 const Chcekout = () => {
@@ -8,9 +12,27 @@ const Chcekout = () => {
     // const [services, setServices] = useService();
     const location = useLocation();
 
+    const [validated, setValidated] = useState(false);
+
+    const handelCheckOutSubmit = (event) => {
+        event.preventDefault();
+        toast('Thanks for fill up the form');
+        console.log("form submitted");
+    };
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+    };
+
     return (
         <div>
-            <h1 className='text-center'>Thanks for Checking out: {serviceId}</h1>
+            <h1 className='text-center p-2'>Checking out Item no {serviceId}</h1>
             {
                 location?.state?.id ?
                     <div className='container text-center'>
@@ -20,8 +42,85 @@ const Chcekout = () => {
                         <p>{location?.state?.description}</p>
                     </div>
                     :
-                    <div></div>
+                    <div className='container text-center'>
+                        <img className='img-fluid w-50 pb-3' src={checkout} alt="" />
+                    </div>
             }
+
+            <div className='checkout-form w-75 mx-auto p-3'>
+                <Form onSubmit={handelCheckOutSubmit} noValidate validated={validated}>
+                    <Row className="mb-3">
+                        <Form.Group as={Col} md="4" controlId="validationCustom01">
+                            <Form.Label>First name</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                placeholder="First name"
+                                defaultValue="Mark"
+                            />
+                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group as={Col} md="4" controlId="validationCustom02">
+                            <Form.Label>Last name</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                placeholder="Last name"
+                                defaultValue="Otto"
+                            />
+                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+                            <Form.Label>Username</Form.Label>
+                            <InputGroup hasValidation>
+                                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Username"
+                                    aria-describedby="inputGroupPrepend"
+                                    required
+                                />
+                                <Form.Control.Feedback type="invalid">
+                                    Please choose a username.
+                                </Form.Control.Feedback>
+                            </InputGroup>
+                        </Form.Group>
+                    </Row>
+                    <Row className="mb-3">
+                        <Form.Group as={Col} md="6" controlId="validationCustom03">
+                            <Form.Label>City</Form.Label>
+                            <Form.Control type="text" placeholder="City" required />
+                            <Form.Control.Feedback type="invalid">
+                                Please provide a valid city.
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group as={Col} md="3" controlId="validationCustom04">
+                            <Form.Label>State</Form.Label>
+                            <Form.Control type="text" placeholder="State" required />
+                            <Form.Control.Feedback type="invalid">
+                                Please provide a valid state.
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group as={Col} md="3" controlId="validationCustom05">
+                            <Form.Label>Zip</Form.Label>
+                            <Form.Control type="text" placeholder="Zip" required />
+                            <Form.Control.Feedback type="invalid">
+                                Please provide a valid zip.
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                    </Row>
+                    <Form.Group className="mb-3">
+                        <Form.Check
+                            required
+                            label="Agree to terms and conditions"
+                            feedback="You must agree before submitting."
+                            feedbackType="invalid"
+                        />
+                    </Form.Group>
+                    <Button type="submit">Submit form</Button>
+                </Form>
+                <ToastContainer></ToastContainer>
+            </div>
 
 
 
